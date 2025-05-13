d0b0c5909ca4f6ae238e18b17d21a2c7dbfe2b87
Ganpat45 <ganpatlmakvana2001@gmail.com>
2025-05-13 17:27:57 +0530

    FooterUpdation

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -7,7 +7,7 @@ import { IoLocationSharp } from 'react-icons/io5';
 
 const Footer = () => {
   return (
-    <footer className="bg-[#16171A] text-white px-4 md:px-8 lg:px-12 py-6 md:py-8  ">
+    <footer className="bg-[#16171A] text-white px-4 md:px-8 lg:px-12 py-30  ">
       <div className="max-w-7xl mx-auto">
         
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 md:gap-8 lg:gap-12">
